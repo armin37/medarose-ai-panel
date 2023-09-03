80068f45d961992ca6d4ca6dc92eec1064b71f1c
maryam <qasemzademari@gmail.com>
2023-09-04 01:32:22 +0430

    chnge register html,register ts,register module

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+  }
+  ngOnInit(): void {
+    this.form = this.fb.group(
+      {
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword:['',Validators.required],
+      email: ['', [Validators.required, Validators.email,Validators.minLength(4)]]
+    
+      },
+      {
+        //validator:confirmPasswordValidator("password","confirmPassword")
+      }
+    )
+  }
+
+  login() {
+    console.log(this.form.value)
+  }
+  asFormControl(control: AbstractControl): FormControl {
+    return control as FormControl;
+  }
 }
--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -5,6 +5,12 @@ import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 import {CardModule, FormModule, GridModule, ButtonModule} from "@coreui/angular";
 import {IconModule} from "@coreui/icons-angular";
+import {MrButtonComponent} from "../../components/mr-button/mr-button.component";
+import {MrInputComponent} from "../../components/mr-input/mr-input.component";
+import {MrPasswordComponent} from "../../components/mr-password/mr-password.component";
+import {MrInputNumberComponent} from "../../components/mr-input-number/mr-input-number.component";
+import {MrSelectComponent} from "../../components/mr-select/mr-select.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -18,7 +24,13 @@ import {IconModule} from "@coreui/icons-angular";
     CardModule,
     FormModule,
     ButtonModule,
-    IconModule
+    IconModule,
+    MrButtonComponent,
+    MrInputComponent,
+    MrPasswordComponent,
+    MrInputNumberComponent,
+    MrSelectComponent,
+    ReactiveFormsModule
   ]
 })
 export class RegisterModule { }
